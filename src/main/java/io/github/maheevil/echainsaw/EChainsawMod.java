5fca6a2ce9dd9c8f00f017c3dda46fabdfc46d00
MaheEvil <maheeshasithum@gmail.com>
2021-07-14 22:17:46 +0530

    added a gamerule to disable creeper mob grieifing,definitely will change how i do this later

--- src/main/java/io/github/maheevil/echainsaw/EChainsawMod.java
+++ src/main/java/io/github/maheevil/echainsaw/EChainsawMod.java
@@ -1,14 +1,38 @@
 package io.github.maheevil.echainsaw;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.world.GameRules;
+import net.minecraft.world.World;
+
+import java.util.Scanner;
 
 public class EChainsawMod implements ModInitializer {
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+	//public static boolean hj;
+	//public int h = 1;
+	//@Override
+	public static final GameRules.Key<GameRules.BooleanRule>
+	CREEPER_GRIEFING = register("creeper");
 
-		System.out.println("Hello Fabric world!");
+
+	private static GameRules.Key<GameRules.BooleanRule> register(String creeper) {
+		return GameRuleRegistry.register(creeper, GameRules.Category.MOBS, GameRuleFactory.createBooleanRule(true));
+	}
+
+
+
+
+	public void onInitialize() {
+
+		/*System.out.println("Hello Fabric world!");
+		String h = new Scanner(System.in).nextLine();
+		if(h.equalsIgnoreCase("h")){
+			this.h = 3;
+			hj=true;
+		}
+		System.out.println(this.h);*/
+
 	}
 }
---
+++ src/main/java/io/github/maheevil/echainsaw/mixin/CreeperMixin.java
@@ -0,0 +1,43 @@
+package io.github.maheevil.echainsaw.mixin;
+
+import io.github.maheevil.echainsaw.EChainsawMod;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.CreeperEntity;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CreeperEntity.class)
+public class CreeperMixin extends HostileEntity {
+	//private Explosion.DestructionType destructionType;
+	//@Shadow Explosion.DestructionType destructionType;
+
+
+	//@Inject(at = @At("HEAD"), method = "explode")
+	//public void explode(CallbackInfo ci){
+		//return;
+	//}
+	//@Shadow World world;
+
+	protected CreeperMixin(EntityType<? extends HostileEntity> entityType, World world) {
+		super(entityType, world);
+	}
+
+	@ModifyVariable(
+		method = "explode",
+			at = @At(value = "STORE"),
+			index = 1
+	)
+	private Explosion.DestructionType destructionType(Explosion.DestructionType destructionType){
+		if(!this.world.getGameRules().getBoolean(EChainsawMod.CREEPER_GRIEFING))
+			return Explosion.DestructionType.BREAK;
+		return destructionType;
+	}
+
+}
